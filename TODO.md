474d7c1c3960a5e265fc5bedd8d61e4a1c80c4ef
marco <marco1101777@gmail.com>
2024-09-02 00:19:12 -0600

    add

---
+++ TODO.md
@@ -0,0 +1,13 @@
+# Extension para  hacer registro diario en hoyolab y otras cosas sobre genshin 
+
+
+# fix 
+- icon characters 
+- name characters
+- canjeo de codigos unlink
+- registro diario
+
+## added :
+- registro diario 
+- materiales de acension (armas) 
+
---
+++ scrip.js
@@ -0,0 +1,176 @@
+const materials = {
+    "Freedom":  {
+       "characters" : ["Amber" ,"Barbara" ,"Klee" , "Sucrose" ,"Diona","Tartaglia","Aloy"] ,  
+       "days" :["Monday" ,"thursday","Sunday"] 
+    },
+    "Resistance" : {
+        "characters" : ["Bennett","Diluc","Jean","Mona","Noelle","Razor","Eula"] ,
+        "days": ["Tuesday","Friday","Sunday"]  
+    },
+    "Ballad" : {
+        "characters":["Venti","Lisa","Fischl","Kaeya","Albedo","Rosaria","Mika"],
+        "days" : ["Wednesday","Saturday","Sunday"] 
+    },
+    "Prosperity":{
+        "characters" : ["Qiqi","Keqing","Ningguang","Xiao","Yelan","Shenhe","Gaming"] ,
+        "days" : ["Monday","Thursday","Sunday"] 
+    },
+    "Diligence":{
+        "characters":["Xiangling","Chongyun","Hu tao","Ganyu","Kazuha","Yun Jin","yaoyao"], 
+        "days" : ["Tuesday","Friday","Sunday"] 
+    },
+    "Gold" : {
+        "characters":["Beidou","Xingqiu","Zhongli","Xinyan","Yanfei","Baizhu","Xianyun"],
+        "days" : ["Wednesday","Saturday","Sunday"] 
+    },
+    "Transience" : {
+        "characters" : ["Yoimiya", "Thoma", "kokomi", "Shikanoin Heizou" , "Kirara" ] ,
+        "days" :["Monday","Thursday","Sunday"] 
+    },
+    "Elegance": {
+        "characters" : ["Ayaka","arataki-itto","Sara","Ayato" ,"Kuki Shinobu"] ,
+        "days" :["Tuesday","Friday","Sunday"] 
+    },
+    "Light" : {
+        "characters" : ["Sayu" , "Gorou","Raiden","Yae Miko","Chiori"] ,
+        "days" : ["Wednesday", "Saturday","Sunday"] 
+    },
+    "Admonition" : {
+        "characters" : ["Cyno","Candace","Tighnari","Faruzan"] ,
+        "days" : ["Monday","Thursday","Sunday"] 
+
+    },
+    "Ingenuity" : {
+        "characters" : ["Dori","Nahida","alhaitham","layla","Kaveh"],
+        "days" : ["Tuesday","Friday","Sunday"] 
+    },
+    "Praxis" : {
+        "characters" : ["Collei" , "Nilou","Dehya","Wanderer","Sethos"] , 
+        "days" : ["Wednesday","Saturday","Sunday"] 
+    },
+    "Equity":{
+        "characters":["lyney","Neuvillette","Navia","Sigewinne"],  
+        "days" : ["Monday","Thursday","Sunday"] 
+    },
+    "Justice" : {
+        "characters" : ["Freminet","Clorinde","Charlotte","Furina"],
+        "days" : ["Tuesday","Friday","Sunday"] 
+    },
+    "Order": {
+        "characters" : ["Wriothesley","Lynette","Chevreuse","Arlecchino","Emilie"], 
+        "days" : ["Wednesday","Saturday","Sunday"] 
+    }
+}
+
+
+const $ = sel =>  document.querySelector(sel) 
+
+const $$ = sel =>  document.querySelectorAll(sel)
+const getId = id =>document.getElementById(id) 
+const consol = getId("consol")
+
+
+
+const registroBtn = $('.registro') ; 
+
+// buttons 
+registroBtn.addEventListener('click' , () => { 
+    window.open('https://www.hoyolab.com/') ;
+})
+
+const codigosBtn = $('.btnCodigos')
+const code = getId('codesList')
+const codes = [
+    "GENSHINGIFT", 
+    "SBNBUK67M37Z",
+]
+codes.forEach(element => {
+    const option = document.createElement("option")
+    option.value = element ; 
+    option.innerText = element ; 
+    code.appendChild(option) 
+
+});
+codigosBtn.addEventListener('click' , () => {
+    window.open(`https://genshin.hoyoverse.com/es/gift?code=${code.value}`) ; 
+
+})
+
+const parseCharacter = (character) => {
+    character = character.replace( /\s/,'-');
+    return character.toLowerCase() 
+    
+}
+
+// load icons characters 
+// api genshin 
+//
+const getImg = (ch) => {
+    ch = parseCharacter(ch) ; 
+    return `https://genshin.jmp.blue/characters/${ch}/icon-side`
+}
+function toBase64(imageUrl, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        const reader = new FileReader();
+        reader.onloadend = function() {
+            callback(reader.result);
+        };
+        reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', imageUrl);
+    xhr.responseType = 'blob';
+    xhr.send();
+}
+
+function saveImageToLocalStorage(url,nameCharacter) {
+    toBase64(url, function(base64Image) {
+        localStorage.setItem(nameCharacter, base64Image);
+    });
+}
+function displayImageFromLocalStorage(nameCharacter) {
+    const base64Image = localStorage.getItem(nameCharacter);
+    if (base64Image) {
+        const img = document.createElement('img');
+        img.src = base64Image;
+        return img 
+    }else{
+        toBase64(getImg(nameCharacter),function(base64Image) {
+            localStorage.setItem(nameCharacter, base64Image);
+        });
+         const img = document.createElement('img');
+        img.src = base64Image;
+        return img 
+
+    }
+}
+
+
+
+const days = ["Sunday","Monday" , "Tuesday" , "Wednesday","Thursday","Friday" ,"Saturday"] 
+//  get day and  show a character talents for day   
+function getCharactersOfDay(){
+    const  day = new Date().getDay() ;
+    const characters = [] ;
+    Object.entries(materials).forEach(([key, value]) => {
+        const ifValue = value.days.find((daysMaterial) => daysMaterial == days[day] );
+        if (ifValue){
+            const ch = value.characters ;
+            characters.push(...ch) ;
+        }
+    });
+    const info = $('.info') ;
+    characters.forEach(name => {
+        const img = displayImageFromLocalStorage(name)
+        const div = document.createElement('div') 
+        div.classList.add("character")
+        div.title = name ;
+        img.classList.add("character-icon")
+        div.appendChild(img)
+        info.appendChild(div) ;
+
+    });
+    const today =getId('day').innerHTML = days[day]
+}
+
+getCharactersOfDay()
