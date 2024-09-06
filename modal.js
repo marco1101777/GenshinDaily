4ee5ac99d713c60e7d2a91bf6aa7bc879074c6c7
marco110177 <marco110177@gmail.com>
2024-09-05 21:45:33 -0600

    fix(modal-codes)

--- modal.js
+++ modal.js
@@ -1,29 +1,32 @@
 class Modal{
    
     constructor(elementHtml){
-    this.style = `
-        position:fixed; 
-        width:100% ; 
-        height : 100vh  ; 
-        display:none ; 
-        justify-content :center ; 
-        align-items : center ;
-        background-color : rgba(0,0,0,0.5) ;
-
-    ` 
+        this.style = `
+            position:fixed; 
+            width:100% ; 
+            height : 100vh  ; 
+            display:none ; 
+            justify-content :center ; 
+            align-items : center ;
+            background-color : rgba(0,0,0,0.5) ;
+        ` 
         this.modalElement = elementHtml ;
         this.modalElement.style = this.style ; 
+        document.addEventListener('keydown' , (e) => {
+            if (e.key == "Escape"){
+                this.close()
+            }
+
+        })
+
     }
 
     close(){
-        console.log(this.modalElement)
         this.modalElement.style.display = "none" 
     }
     showModal(){
         this.modalElement.style.display = "flex"
     }
     
-
-
 }
 
